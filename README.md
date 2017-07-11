766962577ab65ed2bf1bab5a0d5f4f29438079b3
initial commit <geraldoramos@GRamos-Macbook.local>
2017-07-11 16:29:59 -0600

    First commit!

---
+++ README.md
@@ -0,0 +1,2 @@
+# face-detector
+A simple NPM module to detect recognizable faces in an image url or file(path)
---
+++ index.js
@@ -0,0 +1,42 @@
+const axios = require('axios')
+const fs = require('fs')
+const cv = require('opencv')
+
+exports.check = function(path, cb) {
+
+  if (!/^(f|ht)tps?:\/\//i.test(path)) {
+    cv.readImage(path, function(err, im) {
+      im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+        cb(faces.length)
+      });
+    });
+  } else {
+    var filename = path.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+    axios({
+        method: 'get',
+        url: path,
+        responseType: 'stream'
+      })
+      .then(function(response) {
+
+        var stream = response.data.pipe(fs.createWriteStream(filename))
+
+        stream.on('finish', function() {
+          cv.readImage(filename, function(err, im) {
+            im.detectObject(cv.FACE_CASCADE, {}, function(err, faces) {
+              fs.unlink(filename, function(error) {
+                if (error) {
+                  cb('Unable to delete file')
+                }
+                cb(faces.length)
+              })
+            });
+          });
+
+        });
+      });
+  }
+
+
+
+}
